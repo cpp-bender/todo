4967220d4ab45157057019beabd32084346e8e73
Ahmet Altioglu <ahmetaltioglu@hotmail.com>
2024-12-07 21:24:44 +0300

    add clear all todos

--- todo.js
+++ todo.js
@@ -14,90 +14,97 @@ secondCardBody.addEventListener("click", deleteTodo);
 
 filter.addEventListener("keyup", filterTodos);
 
-function filterTodos(e){
-    const filterValue = e.target.value.toLowerCase();
-    const listItems = document.querySelectorAll(".list-group-item");
+clearButton.addEventListener("click", clearAllTodos);
 
-    listItems.forEach(function(item){
-        const text = item.textContent.toLowerCase();
-        if(text.indexOf(filterValue) === -1){
-            item.setAttribute("style","display : none !important");
-        }
-        else{
-            item.setAttribute("style", "display : block");
-        }
-
-    });
+function clearAllTodos(e) {
+  if (confirm("Are you sure, you want to delete all?")) {
+    do {
+      todoList.removeChild(todoList.firstElementChild);
+    } while (todoList.firstElementChild != null);
+    localStorage.removeItem("todos");
+  }
 }
 
-function deleteTodo(e){
-    if(e.target.className === "fa fa-remove"){
-        e.target.parentElement.parentElement.remove();
-        const text = e.target.parentElement.parentElement.textContent;
-        const todos = JSON.parse(localStorage.getItem("todos"));
-        for(let i=0; i<todos.length;i++){
-            if(todos[i] === text){
-                todos.splice(i, 1);
-            }
-        }
-        localStorage.setItem("todos",JSON.stringify(todos));
-        showAlert("success", "Todo removed");
+function filterTodos(e) {
+  const filterValue = e.target.value.toLowerCase();
+  const listItems = document.querySelectorAll(".list-group-item");
+
+  listItems.forEach(function (item) {
+    const text = item.textContent.toLowerCase();
+    if (text.indexOf(filterValue) === -1) {
+      item.setAttribute("style", "display : none !important");
+    } else {
+      item.setAttribute("style", "display : block");
     }
+  });
 }
 
-function loadAllTodos(e){
+function deleteTodo(e) {
+  if (e.target.className === "fa fa-remove") {
+    e.target.parentElement.parentElement.remove();
+    const text = e.target.parentElement.parentElement.textContent;
     const todos = JSON.parse(localStorage.getItem("todos"));
-    for(let i = 0; i<todos.length; i++){
-        addTodoToUI(todos[i]);
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i] === text) {
+        todos.splice(i, 1);
+      }
     }
-}
-
-function addTodo(e){
-    e.preventDefault();
-    const newTodo =  todoInput.value.trim();
-
-    if(newTodo === ""){
-        showAlert("danger", "Type a todo");
-    }
-    else{
-        addTodoToUI(newTodo);
-        addTodoToStorage(newTodo);
-        showAlert("success", "Success!");
-    }
-}
-
-function addTodoToStorage(todo){
-    let todos;
-    if(localStorage.getItem("todos") === null){
-        todos = [];
-    }
-    else{
-        todos =JSON.parse(localStorage.getItem("todos")); 
-    }
-    todos.push(todo);
     localStorage.setItem("todos", JSON.stringify(todos));
+    showAlert("success", "Todo removed");
+  }
 }
 
-function showAlert(type, mes){
-    const alert = document.createElement("div");
-    alert.className = `alert alert-${type}`;
-    alert.innerHTML = `<strong>${mes}</strong>`;
-    firstCardBody.appendChild(alert);
-
-    window.setTimeout(function(){
-        alert.remove();
-    }, 1000);
+function loadAllTodos(e) {
+  const todos = JSON.parse(localStorage.getItem("todos"));
+  for (let i = 0; i < todos.length; i++) {
+    addTodoToUI(todos[i]);
+  }
 }
 
-function addTodoToUI(newTodo){
-    const listItem = document.createElement("li");
-    const link = document.createElement("a");
-    link.href = "#";
-    link.className = "delete-item";
-    link.innerHTML = "<i class = 'fa fa-remove'></i>";
-    listItem.className = "list-group-item d-flex justify-content-between";
-    listItem.appendChild(document.createTextNode(newTodo));
-    listItem.appendChild(link);
-    todoList.appendChild(listItem);
-    todoInput.value = "";
+function addTodo(e) {
+  e.preventDefault();
+  const newTodo = todoInput.value.trim();
+
+  if (newTodo === "") {
+    showAlert("danger", "Type a todo");
+  } else {
+    addTodoToUI(newTodo);
+    addTodoToStorage(newTodo);
+    showAlert("success", "Success!");
+  }
+}
+
+function addTodoToStorage(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function showAlert(type, mes) {
+  const alert = document.createElement("div");
+  alert.className = `alert alert-${type}`;
+  alert.innerHTML = `<strong>${mes}</strong>`;
+  firstCardBody.appendChild(alert);
+
+  window.setTimeout(function () {
+    alert.remove();
+  }, 1000);
+}
+
+function addTodoToUI(newTodo) {
+  const listItem = document.createElement("li");
+  const link = document.createElement("a");
+  link.href = "#";
+  link.className = "delete-item";
+  link.innerHTML = "<i class = 'fa fa-remove'></i>";
+  listItem.className = "list-group-item d-flex justify-content-between";
+  listItem.appendChild(document.createTextNode(newTodo));
+  listItem.appendChild(link);
+  todoList.appendChild(listItem);
+  todoInput.value = "";
 }
