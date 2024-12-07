ec05b624240c8c4b4e7c175d43cd94744e368b27
Ahmet Altioglu <ahmetaltioglu@hotmail.com>
2024-12-07 18:26:40 +0300

    add submit functionality

---
+++ todo.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const filter = document.querySelector("#filter");
+const clearButton = document.querySelector("#clear-todos");
+
+
+form.addEventListener("submit", addTodo);
+
+function addTodo(e){
+    e.preventDefault();
+    const newTodo =  todoInput.value.trim();
+    const listItem = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = "#";
+    link.className = "delete-item";
+    link.innerHTML = "<i class = 'fa fa-remove'></i>";
+    listItem.className = "list-group-item d-flex justify-content-between";
+    listItem.appendChild(document.createTextNode(newTodo));
+    listItem.appendChild(link);
+    todoList.appendChild(listItem);
+    todoInput.value = "";
+}
+
